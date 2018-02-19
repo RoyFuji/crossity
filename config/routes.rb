b60470a9df6266d52b8ded0c0685410f239c23dc
RoyFuji <ryofuji3@yahoo.co.jp>
2018-02-19 14:24:37 +0900

    New

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root "top#index"
+  get "/answers/:id", to: "answers#new"
+  patch "/questions/:id", to: "questions#update"
+  resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:show, :edit, :update]
+  resources :questions, only:[:create, :show, :new]
+  resources :answers, only: [:new, :create, :edit, :update]
+  resources :participants, only: [:create, :update, :show, :destroy]
+  resources :asks, only: [:create, :update, :show]
+
+
+end
+
